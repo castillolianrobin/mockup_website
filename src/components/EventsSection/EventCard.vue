c902d0ef01e375b79562928d9d0320438795d5b9
Lian Robin Castillo <castillolianrobin@gmail.com>
2022-04-15 04:48:50 +0800

    add multiple event images

--- src/components/EventsSection/EventCard.vue
+++ src/components/EventsSection/EventCard.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="rounded bg-white p-5 flex flex-col md:flex-row shadow">
-    <img src="@/assets/events/event-1.jpg" class="h-36" />
+    <img :src="img" class="h-36" />
     <div class="mt-5 md:mt-0 md:px-4 flex flex-col">
-      <h6 class="font-bold">Lorem Ipsum Set Dolor</h6>
-      <p class="my-1 text-sm text-gray-500">Jan 27, 2022</p>
+      <h6 class="font-bold">{{ title }}</h6>
+      <p class="my-1 text-sm text-gray-500">{{ date }}</p>
       <p class="text-sm">
-        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam nihil iure reprehenderit qui in ea voluptate velit esse quam
-        nihil...
+        {{ description }}
       </p>
       <p class="mt-2 md:mt-auto cursor-pointer text-sm text-orange-500 hover:text-black transition">See more</p>
     </div>
@@ -17,6 +15,13 @@
 <script>
 export default {
   name: "EventCard",
+
+  props: {
+    img: String,
+    title: String,
+    date: String,
+    description: String,
+  }
 };
 </script>
 
--- src/components/EventsSection/EventsSection.vue
+++ src/components/EventsSection/EventsSection.vue
@@ -2,10 +2,19 @@
   <div class="bg-blue-800 p-10 py-20">
     <div class="text-white text-center">
       <h2 class="text-2xl font-semibold">Events</h2>
-      <h5 class="mt-1 font-light">Join Upcoming CPD Accredited Training, Events and Seminars</h5>
+      <h5 class="mt-1 font-light">
+        Join Upcoming CPD Accredited Training, Events and Seminars
+      </h5>
     </div>
     <div class="mt-12 grid md:grid-cols-2 gap-5">
-      <EventCard v-for="event in 4" :key="event"></EventCard>
+      <EventCard
+        v-for="(event, index) in events"
+        :key="`event-${index}`"
+        :img="event.img"
+        :title="event.title"
+        :date="event.date"
+        :description="event.description"
+      ></EventCard>
     </div>
 
     <AppButton outline color="white" class="block mx-auto mt-10">
@@ -15,14 +24,45 @@
 </template>
 
 <script>
-import AppButton from '../app/AppButton.vue'
-import EventCard from './EventCard.vue'
-  export default {
+import AppButton from "../app/AppButton.vue";
+import EventCard from "./EventCard.vue";
+export default {
   components: { EventCard, AppButton },
-    
-  }
+  data() {
+    return {
+      events: [
+        {
+          img: require("@/assets/events/event-1.jpg"),
+          title: "Lorem Ipsum Set Dolor",
+          date: "Jan 27, 2022",
+          description:
+            "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil iure reprehenderit qui in ea voluptate velit esse quam nihil...",
+        },
+        {
+          img: require("@/assets/events/event-2.jpg"),
+          title: "Lorem Ipsum Set Dolor",
+          date: "Jan 27, 2022",
+          description:
+            "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil iure reprehenderit qui in ea voluptate velit esse quam nihil...",
+        },
+        {
+          img: require("@/assets/events/event-3.jpg"),
+          title: "Lorem Ipsum Set Dolor",
+          date: "Jan 27, 2022",
+          description:
+            "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil iure reprehenderit qui in ea voluptate velit esse quam nihil...",
+        },
+        {
+          img: require("@/assets/events/event-4.jpg"),
+          title: "Lorem Ipsum Set Dolor",
+          date: "Jan 27, 2022",
+          description:
+            "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil iure reprehenderit qui in ea voluptate velit esse quam nihil...",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
